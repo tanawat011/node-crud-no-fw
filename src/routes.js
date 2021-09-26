6efa8635776d8a34fa042df3bd670dd5a3ed66d9
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2021-09-26 16:37:26 +0700

    Made function for create and initial route

--- src/routes.js
+++ src/routes.js
@@ -1,43 +1,50 @@
 const { HTTP_STATUS } = require('./constants')
 const crud = require('./helpers/crud')
 
-const routes = {
-  '/api/v1/tables': {
-    post: async (req, res) => {
-      await crud.create('tables', req.body)
-      res.statusCode = HTTP_STATUS.CREATED
-      res.write('Created table')
-    },
-    get: async (req, res) => {
-      res.statusCode = HTTP_STATUS.SUCCESS
-      res.write('Get table list')
-    },
-  },
-  '/api/v1/tables/:id': {
-    get: async (req, res) => {
-      res.statusCode = HTTP_STATUS.SUCCESS
-      res.write('Get table by id ' + req.params.id)
-    },
-  },
-  '/api/v1/tables/:id/columns': {
-    get: async (req, res) => {
-      res.statusCode = HTTP_STATUS.SUCCESS
-      res.write('Get column list by table id ' + req.params.id)
-    },
-  },
-  '/api/v1/tables/:id/columns/:columnId': {
-    get: async (req, res) => {
-      res.statusCode = HTTP_STATUS.SUCCESS
-      res.write(`Get column by ${req.params.columnId} and table ${req.params.id}`)
-    },
-  },
-  '/api/v1/tables/:id/:columnId': {
-    get: async (req, res) => {
-      res.statusCode = HTTP_STATUS.SUCCESS
-      res.write(`Get column by ${req.params.columnId} and table ${req.params.id} without path column`)
-    },
-  },
+const routes = {}
+
+const setupPathAndController = (path, controller, method) => {
+  routes[path] = { ...routes[path], [method]: controller }
 }
+
+const route = {
+  get: (path, controller) => setupPathAndController(path, controller, 'get'),
+  post: (path, controller) => setupPathAndController(path, controller, 'post'),
+  put: (path, controller) => setupPathAndController(path, controller, 'put'),
+  patch: (path, controller) => setupPathAndController(path, controller, 'patch'),
+  delete: (path, controller) => setupPathAndController(path, controller, 'delete'),
+}
+
+route.get('/api/v1/tables', (req, res) => {
+  res.statusCode = HTTP_STATUS.SUCCESS
+  res.write('Get table list')
+})
+
+route.post('/api/v1/tables', async (req, res) => {
+  await crud.create('tables', req.body)
+  res.statusCode = HTTP_STATUS.CREATED
+  res.write('Created table')
+})
+
+route.get('/api/v1/tables/:id', (req, res) => {
+  res.statusCode = HTTP_STATUS.SUCCESS
+  res.write('Get table by id ' + req.params.id)
+})
+
+route.get('/api/v1/tables/:id/columns', (req, res) => {
+  res.statusCode = HTTP_STATUS.SUCCESS
+  res.write('Get column list by table id ' + req.params.id)
+})
+
+route.get('/api/v1/tables/:id/columns/:columnId', (req, res) => {
+  res.statusCode = HTTP_STATUS.SUCCESS
+  res.write(`Get column by ${req.params.columnId} and table ${req.params.id}`)
+})
+
+route.get('/api/v1/tables/:id/:columnId', (req, res) => {
+  res.statusCode = HTTP_STATUS.SUCCESS
+  res.write(`Get column by ${req.params.columnId} and table ${req.params.id} without path column`)
+})
 
 module.exports = {
   routes,
